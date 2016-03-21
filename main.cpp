a62afe30ae0509985bea0eb9bf5fa742dd19e999
Teddy Brombach <Teddy@teddys-mbp.dhcp.nd.edu>
2016-03-21 09:19:59 -0400

    Finished normal Random Generator

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <random>
+using namespace std;
 
 int main(){
-    int i =0;
+    
+	default_random_engine generator;
+	normal_distribution<double> distribution(50.0,25.0);
 
-    i = i + 1;
+	int p[200]={};
 
-    cout << i << endl;
+	for(int i=0; i<5000; i++){
+		double number = distribution(generator);
+		++p[int(number)+50];
+	}
+
+	for(int i=0;i<200;i++){
+		cout << i-50 << "-" << (i-50+1) << ": ";
+		cout << std::string(p[i],'*') << endl;
+	}
+
 }
